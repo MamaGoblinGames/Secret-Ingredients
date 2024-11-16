e4abcbbc423ced283e3ecbc83595f8cf4870d659
Eddie Canales <eddie.canales@percipiohealth.com>
2024-11-16 13:20:29 -0500

    Added a simple title screen using UI Toolkit

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        Button startButton = root.Q<Button>("start_button");
+        startButton.clicked += () => LoadGameScene();
+    }
+
+    void LoadGameScene()
+    {
+        SceneManager.LoadScene("TestBox");
+    }
+}
