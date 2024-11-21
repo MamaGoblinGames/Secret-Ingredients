c5686ddf26b5d603b55f5a424dc91ee266bf5032
Eddie Canales <eddie.canales@percipiohealth.com>
2024-11-21 01:19:22 -0500

    Update start screen with logo, image background, and new font

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
+    private IEnumerator coroutine;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,6 +25,18 @@ public class MainMenu : MonoBehaviour
 
         Button startButton = root.Q<Button>("start_button");
         startButton.clicked += () => LoadGameScene();
+
+        coroutine = WaitAndHideLogo(4.0f, root);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator WaitAndHideLogo(float waitTime, VisualElement root)
+    {
+        yield return new WaitForSeconds(waitTime);
+        // root.Q<VisualElement>("MGG_Logo").style.opacity = 0;
+        root.Q<VisualElement>("MGG_Logo").style.top = Length.Percent(100);
+        yield return new WaitForSeconds(1.0f);
+        root.Q<VisualElement>("MGG_Logo").style.display = DisplayStyle.None;
     }
 
     void LoadGameScene()
