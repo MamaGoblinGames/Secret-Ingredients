6ea3bfcd5e8b4c314f70293e413bcbf464fb3bfc
Eddie Canales <eddie.canales@percipiohealth.com>
2024-11-24 19:38:56 -0500

    Add timer to HUD

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,8 +1,26 @@
+using System;
 using Unity.Properties;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public partial class HUD : MonoBehaviour
 {
-    private void OnEnable() {}
+    public HUDData hudData;
+    private void OnEnable() {
+    }
+
+    void Start() {
+    }
+
+    void Update() {
+        if (hudData.timeRemaining > TimeSpan.Zero) {
+            TimeSpan newTimeRemaining = hudData.timeRemaining.Add(TimeSpan.FromSeconds(-Time.deltaTime));
+            if (newTimeRemaining <= TimeSpan.Zero) {
+                newTimeRemaining = TimeSpan.Zero;
+                // UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+            }
+            hudData.SetTimeRemaining(newTimeRemaining);
+            System.Diagnostics.Debug.WriteLine(hudData.formattedTimeRemaining);
+        }
+    }
 }
---
+++ Assets/Scripts/UI/HUDData.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HUDData", menuName = "Scriptable Objects/HUD Data")]
+public class HUDData : ScriptableObject
+{
+    const float defaultLevelDuration = 120f;
+    public TimeSpan timeRemaining = TimeSpan.FromSeconds(defaultLevelDuration);
+    public string formattedTimeRemaining;
+
+    private void SyncTimeRemaining() {
+        formattedTimeRemaining = timeRemaining.ToString(@"mm\:ss");
+    }
+
+    void OnEnable() {
+        ResetTimeRemaining();
+    }
+    void OnValidate() {
+        SyncTimeRemaining();
+    }
+
+    public void ResetTimeRemaining() {
+        timeRemaining = TimeSpan.FromSeconds(defaultLevelDuration);
+        SyncTimeRemaining();
+    }
+    public void SetTimeRemaining(TimeSpan time) {
+        timeRemaining = time;
+        SyncTimeRemaining();
+    }
+}
