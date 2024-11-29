a1ffb8a11f19abae9a7ca2020bc760fb382f5a51
Eddie Canales <eddie.canales@percipiohealth.com>
2024-11-28 23:20:40 -0500

    move mama goblins logo to a splash screen

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,12 +1,9 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
-    private IEnumerator coroutine;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,18 +22,6 @@ public class MainMenu : MonoBehaviour
 
         Button startButton = root.Q<Button>("start_button");
         startButton.clicked += () => LoadGameScene();
-
-        coroutine = WaitAndHideLogo(4.0f, root);
-        StartCoroutine(coroutine);
-    }
-
-    private IEnumerator WaitAndHideLogo(float waitTime, VisualElement root)
-    {
-        yield return new WaitForSeconds(waitTime);
-        // root.Q<VisualElement>("MGG_Logo").style.opacity = 0;
-        root.Q<VisualElement>("MGG_Logo").style.top = Length.Percent(100);
-        yield return new WaitForSeconds(1.0f);
-        root.Q<VisualElement>("MGG_Logo").style.display = DisplayStyle.None;
     }
 
     void LoadGameScene()
