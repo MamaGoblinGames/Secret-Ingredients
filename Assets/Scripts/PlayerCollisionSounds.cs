9f7e3025fcb9a4c7fdedc7ea928ba40f08134d93
Eddie Canales <eddie.canales@percipiohealth.com>
2024-12-27 14:07:51 -0500

    move audio source into player prefab

--- Assets/Scripts/PlayerCollisionSounds.cs
+++ Assets/Scripts/PlayerCollisionSounds.cs
@@ -11,19 +11,29 @@ public class PlayerCollisionSounds : MonoBehaviour
     void Start()
     {
         // get audio source component
-        audioSource = GameObject.Find("Collisions Audio Source").GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if (audioSource == null) return;
 
-        // play sound volume based on collision magnitude
+        // Calculate volume based on collision magnitude.
+        // Volume is 0 when magnitude is less than MIN_MAGNITUDE.
         float magnitude = collision.relativeVelocity.magnitude;
+        float volume = Mathf.Clamp((magnitude - MIN_MAGNITUDE) / (MAX_MAGNITUDE - MIN_MAGNITUDE), 0.0f, 1.0f);
+
+        // Find out if the other object has a collision sound
         ObjectSoundInfo otherSounds = collision.gameObject.GetComponent<ObjectSoundInfo>();
+
+        // If the other object has a collision sound, use that. Otherwise, use the default sound.
         AudioClip audioClip = otherSounds ? otherSounds.collisionSound : collisionSound;
-        audioSource.volume = Mathf.Clamp((magnitude - MIN_MAGNITUDE) / (MAX_MAGNITUDE - MIN_MAGNITUDE), 0.0f, 1.0f);
-        Debug.Log("Collision magnitude: " + magnitude + " Volume: " + audioSource.volume);
+        // Set the volume.
+        audioSource.volume = volume;
+
+        // Play the sound.
         audioSource.PlayOneShot(audioClip);
+
+        Debug.Log("Collision Sound - magnitude: " + magnitude + " Volume: " + audioSource.volume + " Clip: " + audioClip.name);
     }
 }
