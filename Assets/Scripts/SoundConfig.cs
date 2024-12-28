47e64bd596f161b49486805336dd0362fdab1593
Eddie Canales <eddie.canales@percipiohealth.com>
2024-12-28 11:43:38 -0500

    adjust sound config a little

--- Assets/Scripts/SoundConfig.cs
+++ Assets/Scripts/SoundConfig.cs
@@ -8,25 +8,36 @@ public class SoundConfig : ScriptableObject
 
     [Header("Collisions")]
     public AudioClip defaultCollision;
+    [Range(0f, 1f)]
     public float maxCollisionVolume = 1f;
+    [Min(0.0f)]
     public float maxCollisionMagnitude = 40.0f;
+    [Min(0.0f)]
     public float minCollisionMagnitude = 7.0f;
 
     [Header("Particle Pickup")]
     public AudioClip particlePickup;
+    [Range(0f, 1f)]
     public float particlePickupVolume = 0.5f;
+    [Range(0f, 2f)]
     public float particlePickupPitchMax = 1.1f;
+    [Range(0f, 2f)]
     public float particlePickupPitchMin = 0.9f;
 
-    [Header("UI Sounds")]
+    [Header("Pausing")]
     public AudioClip pause;
+    [Range(0f, 2f)]
     public float pausePitch = 1f;
     public AudioClip unpause;
+    [Range(0f, 2f)]
     public float unpausePitch = 2f;
 
-    [Header("Player Sounds")]
+    [Header("Jumping")]
     public AudioClip jump;
+    [Range(0f, 1f)]
     public float maxJumpVolume = 1f;
+    [Range(0f, 2f)]
     public float jumpPitch = 1f;
+    [Header("Charging")]
     public AudioClip charge;
 }
